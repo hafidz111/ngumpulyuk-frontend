4e7ba85edb083cc5fb3d6788832d818cbeeef4ee
hafidz <hnpradigda@gmail.com>
2026-05-27 11:02:54 +0700

    refactor/remove deprecated home dashboard and chat components; eliminate unused functions and streamline codebase for improved maintainability

--- src/application/home/use-cases/get-home-dashboard.js
+++
@@ -1,6 +0,0 @@
-/**
- * @param {{ getHomeDashboard: () => import('../../../domain/home/entities/home-dashboard-content').HomeDashboardContent }} repository
- */
-export function getHomeDashboard(repository) {
-  return repository.getHomeDashboard();
-}
